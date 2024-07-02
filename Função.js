12fd314bde2cdc9dbddbe28d18a1c938aa5fd248
LeonardoKrum <leonardo.krum@hotmail.com>
2024-07-02 20:18:33 -0300

    Realizações de mudanças

--- Função.js
+++ Função.js
@@ -1,56 +1,64 @@
 let notas = [7.5, 8.0, 6.0, 9.5, 5.0, 8.5, 7.0, 10.0, 6.5, 9.0];
 let soma = 0;
+let media = 0;
 let maiorNota = notas[0];
 let menorNota = notas[0];
 let notasAcimaMedia = 0;
 let notasAbaixoMedia = 0;
 
 function total(num) {
-    for (let i = 0; i < notas.length; i++) {
-        soma += notas[i];
-        return soma;
+    for (let i = 0; i < array.length; i++) {
+        soma = soma + array[i];
     }
+    return soma;
+}
+function Media(array) {
+    for (let i = 0; i < array.length; i++) {
+        media += array[i];
+    }
+    media = media / array.length;
+    return media;
 }
 
-function Maior() {
-    for (let i = 0; i < notas.length; i++) {
-        if (notas[i] > maiorNota) {
-            maiorNota = notas[i];
+function Maior(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > maiorNota) {
+            maiorNota = array[i];
         }
     }
     return maiorNota;
 }
 
-function Menor() {
-    for (let i = 0; i < notas.length; i++) {
-        if (notas[i] < menorNota) {
-            menorNota = notas[i];
+function Menor(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < menorNota) {
+            menorNota = array[i];
         }
     }
     return menorNota;
 }
 
-function Acima() {
-    for (let i = 0; i < notas.length; i++) {
-        if (notas[i] >= 6) {
+function Acima(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] >= 6) {
             notasAcimaMedia++;
         }
     }
     return notasAcimaMedia;
 }
-function Abaixo() {
-    for (let i = 0; i < notas.length; i++) {
-        if (notas[i] < 6) {
+function Abaixo(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 6) {
             notasAbaixoMedia++;
         }
     }
     return notasAbaixoMedia;
 }
-function adicionar() {
+function adicionar(array) {
     notas.push(8.0);
 }
 
-function imprimir() {
+function imprimir(array) {
     for (let i = 0; i < notas.length; i++) {
         console.log(notas[i]);
     }
